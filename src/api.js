559e4c04180a122403a818d263fd6a5a961d7546
Mehdi Alam <mehdialam2002@gmail.com>
2026-01-16 18:56:14 +0530

    Init frontend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "https://your-backend.onrender.com/api",
+baseURL: "https://ead-crm-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
